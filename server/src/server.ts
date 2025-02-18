804941bd7899e81721506c0da777952fe03c6352
Bruno Becoski <brunobecoski@gmail.com>
2025-02-17 22:27:47 -0300

    Cria a rota subscribeToEvent

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,38 @@
 import { fastify } from 'fastify'
+import { fastifyCors } from '@fastify/cors'
+import { 
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider
+} from 'fastify-type-provider-zod'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
 
-const app = fastify()
+import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
 
-app.get('/hello', () => {
-  return 'Hello World'
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'NLW Connect',
+      version: '0.0.1',
+    },
+  },
+  transform: jsonSchemaTransform,
 })
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(subscribeToEventRoute)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('🚀 HTTP server running!')
