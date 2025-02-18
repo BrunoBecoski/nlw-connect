b5a6955b40c873b854beb909144e03a287bf5675
Bruno Becoski <brunobecoski@gmail.com>
2025-02-18 20:55:10 -0300

    Cria o componente Input

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,5 +1,6 @@
-import { ArrowRight, Copy } from "lucide-react";
+import { ArrowRight, Copy, Mail, User } from "lucide-react";
 
+import { InputField, InputIcon, InputRoot } from "@/componentes/input";
 import { IconButton } from "@/componentes/icon-button";
 import { Button } from "@/componentes/button";
 
@@ -14,6 +15,27 @@ export default function Home() {
       <IconButton>
         <Copy />
       </ IconButton>
+
+      <InputRoot>
+        <InputIcon>
+          <User className="size-5" />
+        </InputIcon>
+
+        <InputField placeholder="Digite seu nome" />
+      </InputRoot>
+
+      <InputRoot error>
+        <InputIcon>
+          <Mail />
+        </InputIcon>
+
+        <InputField placeholder="Digite seu e-mail" />
+
+        <InputIcon>
+          <Copy />
+        </InputIcon>
+
+      </InputRoot>
     </main>
   );
 }
