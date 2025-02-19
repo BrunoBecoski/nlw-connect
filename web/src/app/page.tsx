c95a893c240f2deac5eb327e150092c808770c43
Bruno Becoski <brunobecoski@gmail.com>
2025-02-18 21:09:42 -0300

    Configura o Biome

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,41 +1,40 @@
 import { ArrowRight, Copy, Mail, User } from "lucide-react";
 
-import { InputField, InputIcon, InputRoot } from "@/componentes/input";
-import { IconButton } from "@/componentes/icon-button";
 import { Button } from "@/componentes/button";
+import { IconButton } from "@/componentes/icon-button";
+import { InputField, InputIcon, InputRoot } from "@/componentes/input";
 
 export default function Home() {
-  return (
-    <main>
-      <Button>
-        Enviar
-        <ArrowRight />
-      </Button>
+	return (
+		<main>
+			<Button>
+				Enviar
+				<ArrowRight />
+			</Button>
 
-      <IconButton>
-        <Copy />
-      </ IconButton>
+			<IconButton>
+				<Copy />
+			</IconButton>
 
-      <InputRoot>
-        <InputIcon>
-          <User className="size-5" />
-        </InputIcon>
+			<InputRoot>
+				<InputIcon>
+					<User className="size-5" />
+				</InputIcon>
 
-        <InputField placeholder="Digite seu nome" />
-      </InputRoot>
+				<InputField placeholder="Digite seu nome" />
+			</InputRoot>
 
-      <InputRoot error>
-        <InputIcon>
-          <Mail />
-        </InputIcon>
+			<InputRoot error>
+				<InputIcon>
+					<Mail />
+				</InputIcon>
 
-        <InputField placeholder="Digite seu e-mail" />
+				<InputField placeholder="Digite seu e-mail" />
 
-        <InputIcon>
-          <Copy />
-        </InputIcon>
-
-      </InputRoot>
-    </main>
-  );
+				<InputIcon>
+					<Copy />
+				</InputIcon>
+			</InputRoot>
+		</main>
+	);
 }
