2c4c281f48a4d2038ac84b3d9b255ead1b72b6a8
Bruno Becoski <brunobecoski@gmail.com>
2025-02-27 22:20:21 -0300

    Cria a rota sendMessage

---
+++ server/src/routes/send-message-route.ts
@@ -0,0 +1,27 @@
+import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import { z } from "zod";
+
+export const sendMessageRoute: FastifyPluginAsyncZod = async (app) => {
+	app.post(
+		"/messages",
+		{
+			schema: {
+				summary: "Send a message to the AI chat",
+				tags: ["ai"],
+				body: z.object({
+					message: z.string(),
+				}),
+				response: {
+					200: z.object({
+						response: z.string(),
+					}),
+				},
+			},
+		},
+		async (request) => {
+			const { message } = request.body;
+
+			return { response: message };
+		},
+	);
+};
